17482b4872403b479684a7eba68c85ca4647ef09
EdgarRosotti <edgarrosotti@hotmail.com>
2021-11-21 20:57:26 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# AF
+
+![AF](https://user-images.githubusercontent.com/13934853/142784160-b28214bf-4caa-4092-b9cc-5a66ac79e485.png)
+## Project setup
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn serve
+```
+
+### Compiles and minifies for production
+```
+yarn build
+```
+
+### Lints and fixes files
+```
+yarn lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-app>
+    <v-system-bar app height="20" style="display: flex; justify-content: center">
+      v-system-bar 
+      </v-system-bar>
+    <v-app-bar app elevation="5" style="display: flex; justify-content: center">
+     v-app-bar
+    </v-app-bar>
+
+    <v-main>
+        <v-row style="height: 100%; margin: 0">
+            <v-navigation-drawer permanent style="display: flex; align-items: center; justify-content: center"
+            >
+              <div style="height: 100%; display: flex; align-items: center;">v-navigation-drawer</div>
+            </v-navigation-drawer>
+            <v-content style="padding: 0">
+              <div style="height: 100%; display: flex; align-items: center; justify-content: center"
+              >v-content</div>
+            </v-content>
+        
+        </v-row>
+    </v-main>
+    <v-footer style="display: flex; justify-content: center">v-footer</v-footer>
+    <v-bottom-navigation style="display: flex; align-items: center;">v-bottom-navigation</v-bottom-navigation>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+};
+</script>
